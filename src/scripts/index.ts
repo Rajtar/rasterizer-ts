ac1e408574a1de3ddda304502b1d42c13ae5cdce
Rajtar <jakubraj@gmail.com>
2020-01-02 20:45:13 +0100

    Add more models to scene

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -10,6 +10,39 @@ import {ObjLoader} from "./io/input/mesh/ObjLoader";
 import {PointLight} from "./light/PointLight";
 import {LightIntensity} from "./light/LightIntensity";
 import {DirectionalLight} from "./light/DirectionalLight";
+import {DrawableObject} from "./geometry/DrawableObject";
+
+function constructMeshes(): DrawableObject[] {
+    const meshLoader = new ObjLoader();
+
+    const monkeyMesh = meshLoader.loadMesh(FileLoader.loadFile("resources/models/monkey.obj"));
+    monkeyMesh.transform.scale(new Vector3(0.75, 0.75, 0.75));
+
+    const cubeMesh = meshLoader.loadMesh(FileLoader.loadFile("resources/models/cube.obj"));
+    cubeMesh.transform.scale(new Vector3(0.5, 0.5, 0.5));
+    cubeMesh.transform.translate(new Vector3(1.75, -1.25, -0.25));
+
+    const octahedronMesh = meshLoader.loadMesh(FileLoader.loadFile("resources/models/octahedron.obj"));
+    octahedronMesh.transform.scale(new Vector3(0.3, 0.3, 0.3));
+    octahedronMesh.transform.translate(new Vector3(-2, 1, 0));
+
+    const dodecahedronMesh = meshLoader.loadMesh(FileLoader.loadFile("resources/models/dodecahedron.obj"));
+    dodecahedronMesh.transform.scale(new Vector3(0.3, 0.3, 0.3));
+    dodecahedronMesh.transform.translate(new Vector3(-2, -1, 0));
+
+    const tetrahedronMesh = meshLoader.loadMesh(FileLoader.loadFile("resources/models/tetrahedron.obj"));
+    tetrahedronMesh.transform.scale(new Vector3(0.3, 0.3, 0.3));
+    tetrahedronMesh.transform.translate(new Vector3(2, 1, 0));
+
+    const sphereMesh = meshLoader.loadMesh(FileLoader.loadFile("resources/models/sphere.obj"));
+    sphereMesh.transform.scale(new Vector3(0.015, 0.015, 0.015));
+    sphereMesh.transform.translate(new Vector3(-2, 0, 0));
+
+    const icosahedronMesh = meshLoader.loadMesh(FileLoader.loadFile("resources/models/icosahedron.obj"));
+    icosahedronMesh.transform.scale(new Vector3(0.5, 0.5, 0.5));
+    icosahedronMesh.transform.translate(new Vector3(2, 0, 0));
+    return [monkeyMesh, cubeMesh, octahedronMesh, dodecahedronMesh, tetrahedronMesh, sphereMesh, icosahedronMesh];
+}
 
 function initialize(): void {
     const canvas: HTMLCanvasElement = document.getElementById("screenCanvas") as HTMLCanvasElement;
@@ -22,21 +55,15 @@ function initialize(): void {
     camera.setLookAt(KeyboardInputData.cameraPosition, KeyboardInputData.cameraTarget, new Vector3(0, 1, 0));
     camera.setPerspective(45, 16/9, 0.1, 100);
 
-    const meshLoader = new ObjLoader();
-    const objText = FileLoader.loadFile("resources/models/monkey.obj");
-    const objMesh = meshLoader.loadMesh(objText);
-
-    // objMesh.transform.scale(new Vector3(0.5, 0.5, 0.5));
-    // objMesh.transform.translate(new Vector3(0, 0, -10));
-    // objMesh.transform.translate(new Vector3(-0.5, -0.5, 0));
-
     const ambientLightColor = new LightIntensity(0, 0, 0);
     const specularLightColor = new LightIntensity(1, 0, 1);
     const light1 = new DirectionalLight(KeyboardInputData.lightPosition, ambientLightColor, new LightIntensity(1, 0, 0), specularLightColor, 20);
     const light2 = new DirectionalLight( new Vector3(-3, 0, 1.5), ambientLightColor, new LightIntensity(0, 0, 1), specularLightColor, 20);
-    const light3 = new PointLight( new Vector3(0, 1, 2), ambientLightColor, new LightIntensity(0.05, 0.05, 0.05), new LightIntensity(0.05, 0.05, 0.05), 20);
+    const light3 = new PointLight( new Vector3(0, 1, 2), ambientLightColor, new LightIntensity(0.1, 0, 0.1), new LightIntensity(0.05, 0.05, 0.05), 20);
 
-    const rasterizer: Rasterizer = new Rasterizer(targetScreen, [objMesh], [light1, light2, light3], camera);
+    const lights = [light1, light2, light3];
+    const drawableObjects = constructMeshes();
+    const rasterizer: Rasterizer = new Rasterizer(targetScreen, drawableObjects, lights, camera);
     rasterizer.update();
 }
 
